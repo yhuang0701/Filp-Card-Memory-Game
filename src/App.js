d487988a8506941bc3c7d47ec368068740377d77
Yichen Huang <achen765223419@gmail.com>
2022-10-10 03:25:14 -0700

    Build initial game state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "../img/helmet-1.png" },
+  { "src": "../img/potion-1.png" },
+  { "src": "../img/ring-1.png" },
+  { "src": "../img/scroll-1.png" },
+  { "src": "../img/shield-1.png" },
+  { "src": "../img/sword-1.png" }
+]
+
+
+
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+
+    // initialize turns
+    setTurns(0);
+  }
+
+  console.log(cards);
+  console.log(turns);
+
   return (
     <div className="App">
       <h1>Magic Match Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }

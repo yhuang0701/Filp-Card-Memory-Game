f28370832591362821f8415856f20aca39ec42e5
Yichen Huang <achen765223419@gmail.com>
2022-10-10 16:01:16 -0700

    Build SingleCard component with some CSS style and refactored code in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
   { "src": "../img/helmet-1.png" },
@@ -38,12 +39,7 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map(card => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="../img/cover.png" alt="card back" />
-            </div>
-          </div>
+          <SingleCard card={card} key={card.id} />
         ))}
       </div>
     </div>
